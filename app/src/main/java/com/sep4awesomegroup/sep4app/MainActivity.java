98a616ba3714191e555d0fa6a297509156e126e9
Andrew Ngwira <andrewngwira61@gmail.com>
2018-11-28 10:37:57 +0100

    camera option

--- app/src/main/java/com/sep4awesomegroup/sep4app/MainActivity.java
+++ app/src/main/java/com/sep4awesomegroup/sep4app/MainActivity.java
@@ -1,10 +1,15 @@
 package com.sep4awesomegroup.sep4app;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
     // define spinner
     Spinner sp;
 
-
+    ImageView imageView;
 
     //define array adapter of string type
     ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_profile);
+        setContentView(R.layout.fragment_create_profile);
 
         sp = (Spinner) findViewById(R.id.spinner);
         adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
@@ -30,5 +35,25 @@ public class MainActivity extends AppCompatActivity {
         // set adapter to spinner
         sp.setAdapter(adapter);
 
+        // Button camera
+        Button btnCamera = (Button)findViewById(R.id.btn_camera);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+        imageView.setImageBitmap(bitmap);
     }
 }
