e37126bba299d11d6501f9f59350095c230af548
Andrew Ngwira <andrewngwira61@gmail.com>
2018-11-28 08:41:41 +0100

    Profile_layout_fragment

--- app/src/main/java/com/sep4awesomegroup/sep4app/MainActivity.java
+++ app/src/main/java/com/sep4awesomegroup/sep4app/MainActivity.java
@@ -2,12 +2,33 @@ package com.sep4awesomegroup.sep4app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    // define spinner
+    Spinner sp;
+
+
+
+    //define array adapter of string type
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_profile);
+
+        sp = (Spinner) findViewById(R.id.spinner);
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.gender));
+
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // set adapter to spinner
+        sp.setAdapter(adapter);
+
     }
 }
