f441e1853baa176d8f45fce30571c807fdcbc0b9
Patrik <260026@via.dk>
2018-12-03 13:38:48 +0100

    creating profile

--- app/src/main/java/com/sep4awesomegroup/sep4app/ProfileActivity.java
+++ app/src/main/java/com/sep4awesomegroup/sep4app/ProfileActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -21,38 +22,41 @@ import com.sep4awesomegroup.sep4app.utility.User;
 
 import java.util.Arrays;
 
-public class Profile extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
-    private FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener authlistener;
-    private static final int RC_SIGN_IN = 1;
+    private Spinner sp;
+    private ArrayAdapter<String> adapter;
+    private TextView name;
+    private TextView email;
+    private TextView age;
+    private ViewModel vm;
 
-    // define spinner
-    Spinner sp;
-
-    ImageView imageView;
-
-    //define array adapter of string type
-    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_profile);
 
+        vm = ViewModelProviders.of(this).get(ViewModel.class);
+
+        name = findViewById(R.id.nameText);
+        email = findViewById(R.id.emailText);
+        age = findViewById(R.id.ageText);
+
         //
         sp = (Spinner) findViewById(R.id.spinner);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.gender));
-
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // set adapter to spinner
         sp.setAdapter(adapter);
-
-        // Button camera
-
-
     }
 
+    public void updateProfile(View v){
+        vm.addUser(new User(name.getText().toString(), email.getText().toString(), sp.getSelectedItem().toString(), age.getText().toString()));
+        closeActivity(v);
+    }
 
+    public void closeActivity(View v){
+        finish();
+    }
 }
