e6f9b6ea910f16aeedbab472124964dc9493c273
Patrik <260026@via.dk>
2018-12-11 12:28:03 +0100

    new main layout

--- app/src/main/java/com/sep4awesomegroup/sep4app/UserDAO.java
+++ app/src/main/java/com/sep4awesomegroup/sep4app/UserDAO.java
@@ -5,14 +5,11 @@ import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.Nullable;
-import com.sep4awesomegroup.sep4app.utility.Post;
 import com.sep4awesomegroup.sep4app.utility.User;
 
 public class UserDAO implements IUserDAO{
